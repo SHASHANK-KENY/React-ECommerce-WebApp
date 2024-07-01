f6455156044218db91403e6ea4a2b3480023d771
SHASHANK-KENY <143278713+SHASHANK-KENY@users.noreply.github.com>
2024-07-01 10:17:23 +0530

    Update index.js

--- src/components/BaseLayout/components/Header/index.js
+++ src/components/BaseLayout/components/Header/index.js
@@ -7,6 +7,7 @@ import './index.css';
 const Header = () => (
   <div className="header">
     <h1 id="header-title">Shop Now</h1>
+    <h3 id="header-title1">React ECommerce WebApp developed by SHASHANK KENY for Solar Square</h3>
     <div className="links-header">
       <p><NavLink activeClassName="selected" className="nav-link-header" to="/women">Women</NavLink></p>
       <p><NavLink activeClassName="selected" className="nav-link-header" to="/men">Men</NavLink></p>
